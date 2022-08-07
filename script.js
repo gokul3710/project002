b4f5cd8a9571a7e51ceb7f449731505b967d3d8a
gokul3710 <gokulkrishnawaryar@gmail.com>
2022-08-07 20:29:01 +0530

    swiper-responsive scrollbar

--- script.js
+++ script.js
@@ -3,14 +3,33 @@ window.addEventListener("scroll",() => {
     document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0);
 })
 
+
+
+
+
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
+  slidesPerView: 1,
+  spaceBetween: 20,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  breakpoints:{
+    599: {
+      slidesPerView: 2,
+      spaceBetween: 40
+    },
+    1023: {
+      slidesPerView: 3,
+      spaceBetween: 60
+    }
   }
 });
+
+
+
+
+
 
 const nav = document.querySelector('.nav-links');
 const openNavBtn = document.querySelector('#nav-toggle-open');
@@ -21,16 +40,24 @@ const openNav = () => {
   openNavBtn.style.display = 'none';
   closeNavBtn.style.display = 'inline-block';
 }
-
 openNavBtn.addEventListener('click', openNav);
+
 
 const closeNav = () => {
   nav.style.display = "none";
   openNavBtn.style.display = 'inline-block';
   closeNavBtn.style.display = 'none';
 }
-
 closeNavBtn.addEventListener('click', closeNav);
+
+if(document.body.clientWidth < 1024){
+  nav.querySelectorAll('li a').forEach(navLink =>{
+    navLink.addEventListener('click', closeNav);
+  })
+}
+
+
+
 
 const textButtons = document.querySelectorAll('.contact-btn');
 
